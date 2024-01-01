50194ffba85fdf454d4865e87c277b88cf5eb6c7
quankquank <nminhquan210599@gmail.com>
2024-01-01 17:09:12 +0700

    Module 01

---
+++ module_01/module_01_workshop/module_01_workshop/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace module_01_workshop
+{
+    class Program
+    {
+        static void hello_world()
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        static void Main(string[] args)
+        {
+            // Hello world
+            Console.WriteLine("Hello World!");
+
+            // Hello world with variables
+            string hello = "Hello ";
+            string world = "World!";
+            string result = hello + world;
+            Console.WriteLine(result);
+
+            // Hello world with input
+            string input = " ";
+            while (input != "Hello World!") {
+                input = Console.ReadLine();
+            }
+            Console.WriteLine(input);
+            
+            // Hello world with function
+            hello_world();
+        }
+    }
+}
